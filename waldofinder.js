522b101354f1c72b5aaef15e4a9c4b66f44595a7
Thomas Li <tom.tk.li@gmail.com>
2017-11-02 19:25:16 +0000

    Cleaned up the code with forEach

--- waldofinder.js
+++ waldofinder.js
@@ -1,14 +1,13 @@
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      index = i;
-      found();   // execute callback
+  arr.forEach(function(element, i) {
+    if (element === "Waldo") {
+      found(i);   // execute callback
     }
-  }
+  });
 }
 
-function actionWhenFound() {
-  console.log("Found Waldo " + "at " + "index " + index );
+function actionWhenFound(found) {
+  console.log("Found Waldo " + "at " + "index " + found );
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
